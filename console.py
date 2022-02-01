2492be34ef020af55351d3db9f3bf4bc401084d4
Sangwani <mightyp2@gmail.com>
2022-02-01 13:17:26 +0200

    debug Console 0.1

--- console.py
+++ console.py
@@ -12,12 +12,13 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_EOF(self, arg):
-        ''' EOF command to exit the program'''
-        exit()
+        ''' EOF signal to exit the program.'''
+        print("")
+        return True
 
     def do_quit(self, arg):
-        """ Quit command to exit the program """
-        exit()
+        """Quit command to exit the program."""
+        return True
 
     def emptyline(self):
         """ Ignores empty lines when passed"""
