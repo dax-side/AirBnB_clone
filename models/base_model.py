877bb376ced91176f0dbb7b7885ba7141b171983
Sangwani <mightyp2@gmail.com>
2022-02-01 22:34:13 +0200

    debug

--- models/base_model.py
+++ models/base_model.py
@@ -32,9 +32,10 @@ class BaseModel():
         self.updated_at = datetime.now()
 
         if len(kwargs) != 0:
+            tform = "%Y-%m-%dT%H:%M:%S.%f"
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
                     self.__dict__[k] = v
         else:
