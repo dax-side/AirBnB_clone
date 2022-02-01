4440aea736e5f72bac19fc888d6a452bfba7633c
Sangwani <mightyp2@gmail.com>
2022-02-01 23:14:05 +0200

    modify console.py

--- console.py
+++ console.py
@@ -5,6 +5,12 @@
 import cmd
 from models.base_model import BaseModel
 import models
+from models.user import User
+from models.state import State
+from models.city import City
+from models.Place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
